2666299f231f4d5e32f022061db766cd97602764
Debug Develop <debugvelop>
2020-04-22 14:45:42 +0700

    fixing if branches

--- Disk Management/fifo-scan-clook_algo.cpp
+++ Disk Management/fifo-scan-clook_algo.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 //fifo
-/*int main(){
+int main(){
     int queueList[20];
     int counter,current,queueSize,head,seek=0,steps;
     float avg;
@@ -26,25 +26,24 @@ using namespace std;
     avg=seek/(float)queueSize;
     cout << "\nAverage Seek Time: " << avg << "\n";
     return 0;
-}*/
+}
 
 //SCAN
-/*int main(){
-    int diskSector[1];
+int main(){
     int current,direction,lastSector,queueSize,request,sectorIndex,head,seek=0,moves=0;
-    bool reset;
     float avg;
-    cout << "SCAN Scheduling Algorithm\n";
+    bool reset;
+    cout << "SCAN Scheduling Algorithm\n\n";
     cout << "Number of Disk Sector: ";
     cin >> lastSector;
-    diskSector[lastSector];
+    int diskSector[lastSector];
     for(int counter=0;counter<lastSector;counter++){
         diskSector[counter]=0;
     }
     cout << "Number of Queue: ";
     cin >> queueSize;
-    cout << "Sector Queue:\n";
     for(int counter=0;counter<queueSize;counter++){
+        cout << "Queue " << counter+1 << ": ";
         cin >> sectorIndex;
         diskSector[sectorIndex]=1;
     }
@@ -59,6 +58,7 @@ using namespace std;
     else{
         reset=true;
     }
+    cout << "\n";
     request=queueSize;
     while(request!=0 || (head!=0 && head!=lastSector-1)){
         if(diskSector[head]==1){
@@ -87,24 +87,22 @@ using namespace std;
     avg=(float)seek/(float)queueSize;
     cout << "\nAverage Seek Time: " << avg << "\n";
     return 0;
-}*/
+}
 
 //C-LOOK
 int main(){
-    int diskSector[1];
-    int base,current,direction,lastSector,out,queueSize,request,in,sectorIndex,head,seek=0,moves=0;
-    bool reset;
+    int base,current,direction,lastSector,queueSize,request,in=0,out=0,sectorIndex,head,seek=0,moves=0;
     float avg;
-    cout << "C-LOOK Scheduling Algorithm\n";
+    bool reset;
+    cout << "C-LOOK Scheduling Algorithm\n\n";
     cout << "Number of Disk Sector: ";
     cin >> lastSector;
-    diskSector[lastSector];
+    int diskSector[lastSector];
     for(int counter=0;counter<lastSector;counter++){
         diskSector[counter]=0;
     }
     cout << "Initial Head Position: ";
     cin >> base;
-    head=current=base;
     cout << "Initial read direction (1. In|2. Out): ";
     cin >> direction;
     if(direction==1){
@@ -115,31 +113,33 @@ int main(){
     }
     cout << "Number of Queue: ";
     cin >> queueSize;
-    cout << "Sector Queue:\n";
     for(int counter=0;counter<queueSize;counter++){
+        cout << "Queue " << counter+1 << ": ";
         cin >> sectorIndex;
         diskSector[sectorIndex]=1;
         if(sectorIndex<base){
-            out+=1;
+            out++;
         }
         else{
-            in+=1;
+            in++;
         }
     }
+    cout << "\n";
     request=queueSize;
+    head=base;
+    current=head;
     while(request!=0){
         if(diskSector[head]==1){
             cout << "Move from " << current << " to " << head << " with " << moves << " seek\n";
-            diskSector[head]=0;
-            current=head;
-            if(diskSector[head]<base){
+            if(head<base){
                 out--;
             }
             else{
                 in--;
             }
+            current=head;
+            diskSector[head]=0;
             moves=0;
-            request--;
         }
         if(reset){
             head--;
@@ -147,11 +147,11 @@ int main(){
         else{
             head++;
         }
-        if(in==0 && out!=0){
+        if(in==0 && !reset){
             reset=true;
             head=base;
         }
-        if(out==0 && in!=0){
+        if(out==0 && reset){
             reset=false;
             head=base;
         }
